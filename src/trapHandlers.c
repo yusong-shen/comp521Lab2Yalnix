0e9f80e6a284302fd96c52cceabaed2d6772d6ab
yusong-shen <yusongtju@gmail.com>
2016-02-24 23:36:18 -0600

    add yalnix.c and trapHandles.c, not it can compile

---
+++ src/trapHandlers.c
@@ -0,0 +1,31 @@
+ #include <comp421/hardware.h>
+ #include <comp421/yalnix.h>
+
+
+void kernel_trap_handler(ExceptionStackFrame *frame) {
+
+}
+
+void clock_trap_handler(ExceptionStackFrame *frame) {
+
+}
+
+void illegal_trap_handler(ExceptionStackFrame *frame) {
+
+}
+
+void memory_trap_handler(ExceptionStackFrame *frame) {
+
+}
+
+void math_trap_handler(ExceptionStackFrame *frame) {
+
+}
+
+void tty_receive_trap_handler(ExceptionStackFrame *frame) {
+
+}
+void tty_transmit_trap_handler(ExceptionStackFrame *frame) {
+
+}
+
